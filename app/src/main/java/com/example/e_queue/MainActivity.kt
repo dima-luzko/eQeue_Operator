3d9a9967494918d9f11fb0e5aa9f79ca773390fe
Медведева Виктория В <v.medvedeva@network.local>
2021-10-18 14:13:59 +0300

    Добавила activity layout

--- app/src/main/java/com/example/e_queue/MainActivity.kt
+++ app/src/main/java/com/example/e_queue/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.e_queue
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.e_queue.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
