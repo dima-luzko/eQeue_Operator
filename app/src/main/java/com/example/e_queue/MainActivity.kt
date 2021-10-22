503811924de2879df772fda7b17c0d34fe7f9492
Медведева Виктория В <v.medvedeva@network.local>
2021-10-22 15:39:02 +0300

    добавлено окно проверки соединения с сервером

--- app/src/main/java/com/example/e_queue/MainActivity.kt
+++ app/src/main/java/com/example/e_queue/MainActivity.kt
@@ -4,16 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.e_queue.databinding.ActivityMainBinding
+import com.example.e_queue.databinding.CheckIpConnectionDialogBinding
 import com.example.e_queue.databinding.ClientRedirectionBinding
 import com.example.e_queue.databinding.SignInBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: SignInBinding
+    private lateinit var binding: CheckIpConnectionDialogBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = SignInBinding.inflate(layoutInflater)
+        binding = CheckIpConnectionDialogBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
